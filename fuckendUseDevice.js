0f8757e1c931e3e8c45e66645f079e8e90d0dd36
Chord <1263178881@qq.com>
2022-10-22 23:55:04 +0800

    fix

--- fuckendUseDevice.js
+++ fuckendUseDevice.js
@@ -1,5 +1,5 @@
 const myStatus = "HTTP/1.1 200 OK";
-const myData = '{"balance":1091.55,"flag":0,"prepayAmount":100,"errmsg":"","returnMoney":0,"spending":0}';
+const myData = '{"balance":100,"flag":0,"prepayAmount":100,"errmsg":"","returnMoney":0,"spending":0}';
 const myResponse = {
     status: myStatus,
     body: myData // Optional.
