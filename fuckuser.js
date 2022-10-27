c62592662906a2998b486687c56072e223d7e7e0
Chord <1263178881@qq.com>
2022-10-27 23:23:02 +0800

    fuck user

--- fuckuser.js
+++ fuckuser.js
@@ -2,7 +2,8 @@ var body =  $request.body;
 var obj = JSON.parse(body);
 if(obj.hasOwnProperty("memberID"))
 {
-    obj["memberID"] = "1018160";
+    var member = 1018000 + Math.ceil(Math.random()*300)
+    obj["memberID"] = member.toString();
     body = JSON.stringify(obj);
 }
 console.log($request.url);
